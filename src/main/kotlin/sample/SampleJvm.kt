bb3b773f81786d2b61dcf3f8a8b7dc9c3d5ef163
Marek Jeszka <121910-mj-@users.noreply.gitlab.com>
2020-08-08 17:26:08 +0200

    Handle promotion (Q by default)

--- src/main/kotlin/sample/SampleJvm.kt
+++ src/main/kotlin/sample/SampleJvm.kt
@@ -1,16 +1,12 @@
 package sample
 
-import com.github.bhlangonijr.chesslib.Board
+import com.github.bhlangonijr.chesslib.*
+import com.github.bhlangonijr.chesslib.move.Move
 import com.github.bhlangonijr.chesslib.move.MoveGenerator
-import io.ktor.application.Application
-import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.application.log
+import io.ktor.application.*
 import io.ktor.http.cio.websocket.Frame
 import io.ktor.http.cio.websocket.readText
-import io.ktor.http.content.file
-import io.ktor.http.content.files
-import io.ktor.http.content.static
+import io.ktor.http.content.*
 import io.ktor.response.respondRedirect
 import io.ktor.routing.Routing
 import io.ktor.routing.get
@@ -22,6 +18,7 @@ import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.delay
 
 val channels = HashSet<SendChannel<Frame>>()
+val board = Board()
 
 fun main() {
     val port = System.getenv("PORT")?.toInt() ?: 8080
@@ -50,16 +47,34 @@ fun Application.module() {
                     is Frame.Text -> {
                         val text = frame.readText()
                         log.info("Received on web socket: $text")
+                        updateBoard(text)
                         channels.filter { it != outgoing }.forEach { it.send(Frame.Text(text)) }
                     }
                 }
             }
-//                outgoing.send(Frame.Text("hello"))
-//                playGame { outgoing.send(Frame.Text(it)) }
         }
         get("/") {
             call.respondRedirect("/static/index.html", permanent = true)
         }
+    }
+}
+
+private fun updateBoard(text: String) {
+    when (text.length) {
+        4 -> board.doMove( // regular move
+            Move(
+                Square.fromValue(text.substring(0, 2).toUpperCase()),
+                Square.fromValue(text.substring(2, 4).toUpperCase())
+            )
+        )
+        5 -> board.doMove( // promotion
+            Move(
+                Square.fromValue(text.substring(0, 2).toUpperCase()),
+                Square.fromValue(text.substring(2, 4).toUpperCase()),
+                if (board.sideToMove == Side.WHITE) Piece.WHITE_QUEEN else Piece.BLACK_QUEEN
+            )
+        )
+        else -> throw IllegalArgumentException("Incorrect move received: $text")
     }
 }
 
